218225d5df1e9de0ecf368412a9ec6aef6e13db6
NamesAreOverrated <65521809+NamesAreOverrated@users.noreply.github.com>
2026-05-31 22:09:36 +0000

    fix deprecation: vim.highlight.on_yank -> vim.hl.hl_op

--- nvim/init.lua
+++ nvim/init.lua
@@ -304,7 +304,7 @@ do
   vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function() vim.highlight.on_yank() end,
+    callback = function() vim.hl.hl_op({ op = 'yank' }) end,
   })
 end
 
