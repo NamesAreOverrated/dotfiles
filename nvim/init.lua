834714e408772a9c85d3436d0d1d710cd7858640
NamesAreOverrated <65521809+NamesAreOverrated@users.noreply.github.com>
2026-05-31 22:57:35 +0000

    version-agnostic yank highlight: use hl_op if available, fall back to highlight.on_yank

--- nvim/init.lua
+++ nvim/init.lua
@@ -359,11 +359,17 @@ do
 
   -- Highlight when yanking (copying) text
   --  Try it with `yap` in normal mode
-  --  See `:help vim.hl.on_yank()`
+  --  See `:help vim.highlight.on_yank()`
   vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function() vim.hl.hl_op({ op = 'yank' }) end,
+    callback = function()
+      if vim.hl.hl_op then
+        vim.hl.hl_op({ op = 'yank' })
+      elseif vim.highlight and vim.highlight.on_yank then
+        vim.highlight.on_yank()
+      end
+    end,
   })
 end
 
